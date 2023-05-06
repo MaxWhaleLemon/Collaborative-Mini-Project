8f96c221e5d67a6bf9dabfa7e4a05705d3eaf3b7
Liamy90239 <131836212+Liam123456789987654321@users.noreply.github.com>
2023-05-06 20:34:41 +0930

    Enemies follow a path just in the complete opposite direction

--- GameScene.gd
+++ GameScene.gd
@@ -5,6 +5,7 @@ var current_wave = 0
 var enemies_in_wave = 0
 
 func _ready():
+	map_node = get_node("TileMap")
 	start_next_wave()
 
 # Called when the node enters the scene tree for the first time.
@@ -14,13 +15,13 @@ func start_next_wave():
 	spawn_enemies(wave_data)
 
 func retrieve_wave_data():
-	var wave_data = [["BlueTank", 0.7], ["BlueTank", 0.1]]
+	var wave_data = [["enemy_test", 0.7], ["enemy_test", 0.1]]
 	current_wave +- 1
 	enemies_in_wave = wave_data.size()
 	return wave_data
 
 func spawn_enemies(wave_data):
 	for i in wave_data:
-		var new_enemy = load("res://Enemies/" + i[0] + ".tscn").instance()
+		var new_enemy = load("res://Enemies/" + i[0] + ".tscn").instantiate()
 		map_node.get_node("Path").add_child(new_enemy, true)
 		await(get_tree().create_timer(0.2).timeout)
