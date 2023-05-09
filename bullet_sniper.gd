339b96f470df81f6b3b797f1d6f1782c27730dbd
Liamy90239 <131836212+Liam123456789987654321@users.noreply.github.com>
2023-05-09 12:40:06 +0930

    Turret tracks enemies

--- bullet_sniper.gd
+++ bullet_sniper.gd
@@ -1,5 +1,6 @@
-extends Sprite2D
+extends Node2D
 
+var bullet_damage = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,5 +8,5 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
 	pass
--- flame_tower.gd
+++ flame_tower.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	turn()
 	
 func turn():
-	var hi = get_tree().get_nodes_in_group("enemy")
-	var first = hi[0]
-	print(first)
+	pass
+	#var hi = get_tree().get_nodes_in_group("enemy")
+	#var first = hi[0]
+	#print(first)
 	
 	#var enemy_position = get_groups(enemy)
-	get_node("Turret").look_at(first.global_position)
+	#get_node("Turret").look_at(first.global_position)
