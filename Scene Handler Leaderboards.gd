5fd28bc21bf38bec07bc20db6d5ed4572bf04b45
MaxWhaleLemon <max@benandmandy.com>
2023-05-11 09:20:32 +0930

    Highscore Works!! Hopefully

--- Scene Handler Leaderboards.gd
+++ Scene Handler Leaderboards.gd
@@ -8,3 +8,11 @@ func _ready():
 
 func on_back_pressed():
 	get_tree().change_scene_to_file("res://scene_handler_main.tscn")
+	
+	
+
+func _physics_process(delta):
+#	$Control/VBoxContainer/HighScore.value = GlobalScript.Highscore
+	$Control/VBoxContainer/HighScore.text = str(100)
+	
+
--- scene_handler_GUI.gd
+++ scene_handler_GUI.gd
@@ -6,4 +6,9 @@ func _ready():
 	pass
 
 func _process(delta):
-	pass
+	$HBoxContainer/InfoBarRight/VBoxContainer/ProgressBar.value = GlobalScript.CurrentHealth
+	$HBoxContainer/InfoBarRight/VBoxContainer/HealthNumber.text = str(GlobalScript.CurrentHealth)
+	$HBoxContainer/InfoBarRight/VBoxContainer/ProgressBar.max_value = GlobalScript.MaxHealth
+	$HBoxContainer/InfoBarLeft/HBoxContainer/RoundNumber.text = str(GlobalScript.Round)
+	
+	
