bbc3924b3b57441282bd1ee89f89ddaf4554d964
MaxWhaleLemon <max@benandmandy.com>
2023-05-18 09:59:41 +0930

    little bit of gaming

--- scene_handler_upgrades.gd
+++ scene_handler_upgrades.gd
@@ -3,14 +3,16 @@ extends Node
 var rand_upgrade = 0
 var upgradeArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 var button1 = upgradeArray[0]
-var button2 = upgradeArray[0]
-var button3 = upgradeArray[0]
-var button4 = upgradeArray[0]
+var button2 = upgradeArray[1]
+var button3 = upgradeArray[2]
+var button4 = upgradeArray[3]
 
 func _ready():
 	#var rand_upgrade = randi() % 18
 	
 	
+	
+	print("upgradeArray c", upgradeArray)
 	randomizeArrayOrder()
 	
 	#_upgrade_button()
@@ -21,8 +23,33 @@ func _ready():
 	print(button4)
 	
 func randomizeArrayOrder():
+	print("upgradeArray a", upgradeArray)
 	upgradeArray.shuffle()
+	print("upgradeArray b", upgradeArray)
 	print(upgradeArray)
+	
+	button1 = upgradeArray[0]
+	button2 = upgradeArray[1]
+	button3 = upgradeArray[2]
+	button4 = upgradeArray[3]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 func _upgrade_button():	
